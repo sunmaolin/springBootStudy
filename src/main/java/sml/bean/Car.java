c27a96a67d7bf46a3dc4afa928fc02f357a11406
sunmaolin <Mr.sun727@iCloud.com>
2021-03-12 22:03:20 +0800

    @Component+@ConfigurationProperties注解加载属性注入

---
+++ src/main/java/sml/bean/Car.java
@@ -0,0 +1,46 @@
+package sml.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+/**
+ * 只有在容器中的组件才会拥有SpringBoot强大的功能
+ */
+@Component
+//该注解加载properties中的文件注入到属性中(只有自动注入autowired，或从容器中获取的才会注入)
+@ConfigurationProperties(prefix = "mycar")
+public class Car {
+    /**
+     * 品牌
+     */
+    private String brand;
+    /**
+     * 价格
+     */
+    private int price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
--- src/main/java/sml/controller/HelloController.java
+++ src/main/java/sml/controller/HelloController.java
@@ -1,13 +1,24 @@
 package sml.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sml.bean.Car;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    Car car;
 
     @RequestMapping("/hello")
     public String hello(){
         return "hello boot";
     }
+
+    @RequestMapping("/car")
+    public Car car(){
+        return car;
+    }
 }
+
